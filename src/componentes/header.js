994cf5288702dfd89db1024fc7a09ff5f3664008
Brenda Mesias Villar <brendamesiasvillar@gmail.com>
2018-08-25 02:05:14 -0500

    agregando cambios

---
+++ src/componentes/header.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function Header(){
+    return (
+    <header> 
+        <span>YATIQIRI</span>
+        <ul>
+            <li>Inicio</li>
+            <li>Bootcamp</li>
+            <li>Comienza</li>
+            <li>Contacto</li>
+        </ul>
+    </header> 
+    )
+}
+
+export default Header
